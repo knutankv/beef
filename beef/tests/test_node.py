9568d720f902763a9a6fc5234ecf778ea0103612
Knut Andreas Kvåle <knutankv@users.noreply.github.com>
2023-06-28 14:54:05 +0200

    Remove old test files

--- beef/tests/test_node.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from ..fe.node import Node
-
-class TestThreeDimNode(unittest.TestCase):
-    _node = Node(1,[0,1,2])
-    assert _node.label == 1
-
-if __name__ == "__main__":
-    unittest.main()
